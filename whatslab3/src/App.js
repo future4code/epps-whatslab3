e182a34f6a790be8c589d0ff5e8c78981d32869f
DanielRatti <danielpr55rr@gmai.com>
2020-12-18 14:50:16 -0300

    Realizado os 2 exercicios e o 1 desafio

--- whatslab3/src/App.js
+++ whatslab3/src/App.js
@@ -1,54 +1,84 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
+import styled from "styled-components"
+import userEvent from '@testing-library/user-event';
 
 
 class App extends React.Component {
   
   state = {
-    mensagens: [
-      {
-      nomeUsuario:"",
-      mensagemUsuario:""
-    }],
-    inputNomeUsuario :'',
-    inputMensagemUsuario :''
+    mensagens: [],
+    inputNomeUsuario: "",
+    InputMensagemUsuario:"",
+  }
+  onChangeInputUsuario = event =>{
+    this.setState({inputNomeUsuario: event.target.value})
+    console.log(event.target.value);
   }
 
-  adicionarMensagem(){
+  onChangeMensagem = event =>{
+    this.setState({inputMensagemUsuario: event.target.value})
+    console.log(event.target.value);
+  }
+  
+   adicionarMensagem=()=>{
     const novaMensagem = {
       nomeUsuario: this.state.inputNomeUsuario,
       mensagemUsuario: this.state.inputMensagemUsuario
+      
     }
 
-    const mensagensNovas = this.state.mensagens
-    mensagensNovas.push(novaMensagem)
+    
+      
 
-    this.setState({mensagens : mensagensNovas})
+
+    const MensagemNova = [...this.state.mensagens, novaMensagem]
+    this.setState({
+      mensagens: MensagemNova,
+        })
+        console.log(this.state.mensagens);
+
+  
 
   }
 
+  onKeyDownInput = event =>{
+    if(event.key === "Enter"){
+      console.log(`tecla: ${event.key}`);
+      this.adicionarMensagem()
+    }
+  }
+  
   
   
   render(){
-     const adicionarMensagem =() =>
-    
-     return (
-      <div className="Pai">
+    const listaMensagem = this.state.mensagens.map((mensagem)=>{
+      return(
+        <div> 
+          <p><strong>{mensagem.nomeUsuario}:</strong> {mensagem.mensagemUsuario}</p>
+        </div>
+      )
+    })
+      
+    return (
+      <div onKeyDown={this.onKeyDownInput} className="Pai">
         <div className= "quadrado">
+          <div className="containerMensagem">{listaMensagem}</div>
 
         </div>
         <div className= "inputs">
-          <input className="nome"
-          placeholder="nome"
+          <input className="Usuario"
+          placeholder="Usuario"
+
+          onChange={this.onChangeInputUsuario}
           />
            <input className="Mensagem"
           placeholder="mensagem"
+          onChange={this.onChangeMensagem}
           />
 
-          <button onClick="">Enviar</button>
+          <button onClick={this.adicionarMensagem}>Enviar</button>
         </div>
 
 
